2c540ab81759070e607b284dc153e0395804a1ca
Dev Karan Singh <dev1604karan@gmail.com>
2024-12-12 20:23:57 +0530

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/auth/loginOrRegister.dart';
+import 'package:food_delivery/firebase_options.dart';
 import 'package:food_delivery/models/restaurant.dart';
 
 import 'package:food_delivery/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiProvider(
       providers: [
